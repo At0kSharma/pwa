5ac112fc63aa8151ceec047d0d88a592f7181065
thakuralok99 <thakuralok99@gmail.com>
2023-12-19 12:02:38 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const inputDate = document.getElementById("input-field");
+const addBtn = document.getElementById("add-btn");
+
+addBtn.addEventListener("click", () => {
+    let inputValue = inputDate.value;
+    console.log(inputValue);
+
+})
